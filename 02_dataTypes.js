3ffe7877f759f3377978f9f688a01c47b79980c5
Nilesh <nileshjadon411@gmail.com>
2026-08-31 11:20:53 +0530

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" ; //treat all JS code as new version
+
+// alert(3+3) works in browser not in node.js
+
+console.log(3+3);
+// code readibility is a must >>>>>
+
+
+console.log("Nilesh");
+
+let name = "Nilesh"
+let age = 22
+let isLoggedIn = true
+
+
+/* 
+PRIMITIVE
+number => 2 to power 52
+bigInt
+String => can be used in single as well as double quotes
+boolean => true or false
+null => standalone value , representation of empty value.  ( let state = null )
+undefined => variable declared but value not defined
+symbol => to find uniqueness , mostly used in react
+
+OBJECT
+
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
