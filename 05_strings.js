e2657543193331b69f378ba37cf5547102176d01
Nilesh <nileshjadon411@gmail.com>
2026-09-01 18:09:21 +0530

    strings

---
+++ 05_strings.js
@@ -0,0 +1,39 @@
+let name = " Nilesh ";
+
+let repo_count = 50;
+
+// console.log(`Hello my name is ${name} and my repo count is ${repo_count}`);
+
+
+const gameName = new String("nilesh-The-OG");
+
+// console.log(gameName[1]);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(8));
+// console.log(gameName.indexOf('T'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+// const anotherString = gameName.slice(-11,9)
+// console.log(anotherString)
+
+
+// const user = "   nilesh  "
+// console.log(user)
+// console.log(user.trim());
+
+
+const url = "https://nilesh.com/nilesh%20singh";
+// console.log(url.replace('%20' , '-'))
+// console.log(url.includes('nilesh'));
+// console.log(url.includes('jadon'));
+
+console.log(gameName.split('-'));
+
+
