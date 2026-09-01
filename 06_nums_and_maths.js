7b260c9e5f674552242d3f33fca1fa5f93b4c4a8
Nilesh <nileshjadon411@gmail.com>
2026-09-01 19:12:13 +0530

    Math and num

---
+++ 06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score =  400;
+// console.log(score);
+
+const balance = new Number(50000);
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const hundreds = 90000000000;
+//  console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+/*
+Math.floor => 4.2 ==> 4
+Math.ceil => 4.2 ==> 5
+
+*/
+
+console.log(Math.random());
+console.log((Math.random()*10 )+ 1);
+console.log(Math.floor((Math.random()*10 )+ 1));
+
+const min = 10;
+const max = 20;
